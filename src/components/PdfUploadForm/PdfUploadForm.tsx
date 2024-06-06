6dded7287fc7a882bf245529c58e642657ab2c49
Phan Huy Hoang <hoangph271@gmail.com>
2024-06-06 22:04:40 +0700

    feat: generate ToC with HTML to PDF

--- src/components/PdfUploadForm/PdfUploadForm.tsx
+++ src/components/PdfUploadForm/PdfUploadForm.tsx
@@ -21,28 +21,39 @@ export default function PdfUploadForm () {
   return (
     <form action={handleUpload} className="flex flex-col gap-4">
       {pdfUrl ? (
-        <iframe src={pdfUrl} width="1200" height="580" />
+        <iframe
+          src={pdfUrl}
+          width="1200"
+          height="580"
+          style={{
+            maxWidth: 'calc(100vw - 4rem)',
+          }}
+        />
       ) : (
         null
       )}
-      <label className="flex flex-col items-center">
-        <p className="flex-col flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          {pdfFilename ? null : (
-            <span className="mb-4">Get started by uploading a PDF file...!</span>
-          )}
-          <input
-            type="file"
-            name="file"
-            accept="application/pdf"
-            ref={fileInput}
-            required
-          />
-        </p>
-      </label>
-      <button
-        type="submit"
-        className="text-white bg-green-700 hover:bg-green-800 rounded-lg px-5 py-2.5 me-2 mb-2"
-      >Submit</button>
+      <div className={`flex ${pdfUrl ? 'justify-center' : 'flex-col'} gap-4`}>
+        <label className="flex flex-col items-center align-center">
+          <p className="flex-col flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 p-4 rounded-2xl backdrop-blur-2xl">
+            {pdfFilename ? null : (
+              <span className="mb-4 text-center">Get started by uploading a PDF file...!</span>
+            )}
+            <input
+              type="file"
+              name="file"
+              accept="application/pdf"
+              ref={fileInput}
+              required
+            />
+          </p>
+        </label>
+        <button
+          type="submit"
+          className={`${pdfUrl ? 'self-center' : ''} text-white bg-green-700 hover:bg-green-800 rounded-lg px-5 py-2.5`}
+        >
+          Submit
+        </button>
+      </div>
     </form>
   );
 }
